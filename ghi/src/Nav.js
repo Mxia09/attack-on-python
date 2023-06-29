5873f1709c8a3535e7ce286ffefabd1b624c3535
Russell Cruz <russellcruz@Russells-Laptop.attlocal.net>
2023-06-29 15:26:50 -0700

    troubleshooting refresh

--- ghi/src/Nav.js
+++ ghi/src/Nav.js
@@ -3,7 +3,7 @@ import { NavLink } from 'react-router-dom';
 function Nav() {
   return (
     <header>
-    <nav className="navbar navbar-expand-lg navbar-dark bg-success">
+    <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
       <div className="container-fluid">
         <NavLink className="navbar-brand" to="/">Attack on Python</NavLink>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -12,11 +12,11 @@ function Nav() {
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
         <ul className="navbar-nav me-auto mb-2 mb-lg-0">
         <li className="nav-item dropdown">
-        <NavLink className="nav-link dropdown-toggle" to="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">Manufacturers</NavLink>
+        <NavLink className="nav-link dropdown-toggle " to="#" role="button" data-bs-toggle="dropdown" aria-expanded="false"> <span className="navbar-toggler-icon"></span></NavLink>
           <ul className="dropdown-menu">
-            <li><NavLink className="dropdown-item" to="/manufacturers/">View Manufacturers</NavLink></li>
+            <li><NavLink className="dropdown-item" to="/">Home</NavLink></li>
             <li><hr className="dropdown-divider"/></li>
-            <li><NavLink className="dropdown-item" to="/manufacturers/new/">Create Manufacturer</NavLink></li>
+            <li><NavLink className="dropdown-item" to="/about-us/">About Us</NavLink></li>
           </ul>
         </li>
         </ul>
