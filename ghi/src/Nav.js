67ce98f2bef9f618a32dd44bddbf9b62c0010831
Russell Cruz <russellcruz@Russells-Laptop.attlocal.net>
2023-06-29 16:12:21 -0700

    fixed refresh

--- ghi/src/Nav.js
+++ ghi/src/Nav.js
@@ -5,7 +5,7 @@ function Nav() {
     <header>
     <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
       <div className="container-fluid">
-        <NavLink className="navbar-brand" to="/">Attack on Python</NavLink>
+        <NavLink className="navbar-brand" to="/"><img src="./logo.svg" alt="Logo" /></NavLink>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
