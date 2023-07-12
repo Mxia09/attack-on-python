4ad9daacb07c25b4de89d28199fbdbd3231aae67
Marvin Xia <marvinxia09@gmail.com>
2023-07-11 21:38:34 -0700

    scores PUT Backend completed

--- api/queries/scores.py
+++ api/queries/scores.py
@@ -31,6 +31,39 @@ class ScoreOut(BaseModel):
     player_10: int
 
 class ScoreRepository:
+    def create(self, score: ScoreIn) -> ScoreOut:
+        try:
+            with pool.connection() as conn:
+                with conn.cursor() as db:
+                    result = db.execute(
+                        """
+                            INSERT INTO scores
+                                (player_1, player_2, player_3, player_4, player_5,
+                                player_6, player_7, player_8, player_9, player_10)
+                            VALUES
+                                (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
+                            RETURNING id;
+                            """,
+                            [
+                                score.player_1,
+                                score.player_2,
+                                score.player_3,
+                                score.player_4,
+                                score.player_5,
+                                score.player_6,
+                                score.player_7,
+                                score.player_8,
+                                score.player_9,
+                                score.player_10,
+                            ]
+                    )
+                    id = result.fetchone()[0]
+                    # old_data = score.dict()
+                    return self.score_in_to_out(id, score)
+        except Exception as e:
+            print(e)
+            return {"message": "Unable to save score"}
+    
     def get_all(self) -> Union[Error, List[ScoreOut]]:
         try:
             with pool.connection() as conn:
@@ -90,38 +123,48 @@ class ScoreRepository:
         except Exception as e:
             print(e)
             return {"message": "Unable to load scores"}
-
-            
         
-    def create(self, score: ScoreIn) -> ScoreOut:
+    def update(self, score_id: int, score: ScoreIn,) -> Union[ScoreOut, Error]:
         try:
             with pool.connection() as conn:
                 with conn.cursor() as db:
-                    result = db.execute(
+                    db.execute(
                         """
-                            INSERT INTO scores
-                                (player_1, player_2, player_3, player_4, player_5,
-                                player_6, player_7, player_8, player_9, player_10)
-                            VALUES
-                                (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
-                            RETURNING id;
-                            """,
-                            [
-                                score.player_1,
-                                score.player_2,
-                                score.player_3,
-                                score.player_4,
-                                score.player_5,
-                                score.player_6,
-                                score.player_7,
-                                score.player_8,
-                                score.player_9,
-                                score.player_10,
-                            ]
+                        UPDATE scores
+                        SET player_1 = %s,
+                            player_2 = %s, 
+                            player_3 = %s, 
+                            player_4 = %s, 
+                            player_5 = %s, 
+                            player_6 = %s,
+                            player_7 = %s ,
+                            player_8 = %s,
+                            player_9 = %s,
+                            player_10 = %s
+                        WHERE id = %s
+                        """,
+                        [
+                            score.player_1,
+                            score.player_2,
+                            score.player_3,
+                            score.player_4,
+                            score.player_5,
+                            score.player_6,
+                            score.player_7,
+                            score.player_8,
+                            score.player_9,
+                            score.player_10,
+                            score_id
+                        ]
                     )
-                    id = result.fetchone()[0]
-                    old_data = score.dict()
-                    return ScoreOut(id=id, **old_data)
+                    # old_data = score.dict()
+                    # return ScoreOut(id=score_id, **old_data)
+                    return self.score_in_to_out(score_id, score)
         except Exception as e:
             print(e)
-            return {"message": "Unable to save score"}
+            return {"message": "Unable to update new score"}
+        
+    def score_in_to_out(self, id: int, score: ScoreIn):
+        old_data = score.dict()
+        return ScoreOut(id=id, **old_data)
+        
--- api/routers/scores.py
+++ api/routers/scores.py
@@ -19,13 +19,17 @@ def create_score(
     return repo.create(score)
     
 
-# @router.get("/scores", response_model=Union[Error, List[ScoreOut]])
-# def get_all(
-#     repo: ScoreRepository = Depends()
-# ):
-#     return repo.get_all(),
-@router.get("/scores", response_model=List[ScoreOut])
-def get_all(
+
+@router.get("/scores", response_model=Union[List[ScoreOut], Error])
+def get_all_scores(
     repo: ScoreRepository = Depends(),
 ):
     return repo.get_all()
+
+@router.put("/scores/{score_id}", response_model=Union[ScoreOut, Error])
+def update_score(
+    score_id: int,
+    score: ScoreIn,
+    repo: ScoreRepository = Depends(),
+) -> Union[Error, ScoreOut]:
+    return repo.update(score_id, score)
