d412754af634756a390f5ec6a1265bdc14247183
Marvin Xia <marvinxia09@gmail.com>
2023-07-21 17:04:19 -0700

    front auth completed

--- ghi/src/LoginForm.js
+++ ghi/src/LoginForm.js
@@ -6,72 +6,27 @@ export default function LoginForm(props) {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const { login } = useToken();
+  const { token } = useToken();
   const navigate = useNavigate();
 
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      // await login(email, password);
-      // console.log("Logged in successfully!");
-      // loginSuccess();
-      // e.target.reset();
-      // navigate("/play");
       await login(email, password);
       props.setShowToast(true);
       props.setToastVariant("success");
       props.setToastMessage("Login Successful");
+      navigate("/play");
     } catch (error) {
-      // console.error("Error during login:", error);
-      // logingError();
       props.setShowToast(true);
       props.setToastVariant("danger");
       props.setToastMessage("Login Unsuccessful");
     }
   };
 
-  //   if (e.ok) {
-  //     await login(email, password);
-  //     props.setShowToast(true);
-  //     props.setToastVariant("success");
-  //     props.setToastMessage("Login Successful");
-  //   } else {
-  //     props.setShowToast(true);
-  //     props.setToastVariant("danger");
-  //     props.setToastMessage("Login Unsuccessful");
-  //   }
-  // };
-
-  // const loginSuccess = props.setshowtoast("Wecome to Attack on Python!", {
-  //   position: "bottom-right",
-  //   autoClose: 2000,
-  //   hideProgressBar: true,
-  //   closeOnClick: true,
-  //   pauseOnHover: true,
-  //   draggable: true,
-  //   progress: undefined,
-  //   theme: "colored",
-  // });
-
-  // const logingError = () =>
-  //   toast.error(
-  //     "There was an error with your login/password; please try again or visit attoackonpython.com/forgot if you need to reset",
-  //     {
-  //       position: "bottom-right",
-  //       utoClose: 2000,
-  //       hideProgressBar: true,
-  //       closeOnClick: true,
-  //       pauseOnHover: true,
-  //       draggable: true,
-  //       progress: undefined,
-  //       theme: "colored",
-  //     }
-  //   );
-
   return (
     <div>
-      <div style={{ position: "fixed", right: 0, width: 200 }}>
-        {props.toast}
-      </div>
+      <div style={{ position: "fixed", right: 0, width: 200 }}></div>
       <div className="row">
         <div className="offset-3 col-6" style={{ padding: 50 }}>
           <div className="shadow-lg p-4 mt-4">
