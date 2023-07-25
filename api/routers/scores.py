74d5819c0fb5606a72534f6b109f562e7ebc8ade
Jorge Landeros De Santiago <landerosjorge8@outlook.com>
2023-07-25 17:32:19 +0000

    Scores

--- api/routers/scores.py
+++ api/routers/scores.py
@@ -5,49 +5,34 @@ from queries.scores import (
     ScoreIn,
     ScoreRepository,
     ScoreOut,)
+from authenticator import authenticator
 
 router = APIRouter()
 
 
-@router.post("/scores", response_model=Union[ScoreOut, Error])
+@router.post("/api/scores", response_model=Union[ScoreOut, Error])
 def create_score(
     score: ScoreIn,
     response: Response,
-    repo: ScoreRepository = Depends()
+    repo: ScoreRepository = Depends(),
+    account_data: dict = Depends(authenticator.get_current_account_data),
     ):
     response.status_code = 200
     return repo.create(score)
 
 
 
-@router.get("/scores", response_model=Union[List[ScoreOut], Error])
+@router.get("/api/scores", response_model=Union[List[ScoreOut], Error])
 def get_all_scores(
     repo: ScoreRepository = Depends(),
 ):
     return repo.get_all()
 
-@router.put("/scores/{score_id}", response_model=Union[ScoreOut, Error])
-def update_score(
-    score_id: int,
-    score: ScoreIn,
-    repo: ScoreRepository = Depends(),
-) -> Union[Error, ScoreOut]:
-    return repo.update(score_id, score)
 
-@router.delete("/scores/{score_id}", response_model=bool)
+@router.delete("/api/scores/{score_id}", response_model=bool)
 def delete_score(
     score_id: int,
     repo: ScoreRepository = Depends(),
+    account_data: dict = Depends(authenticator.get_current_account_data),
 ) -> bool:
     return repo.delete_score(score_id)
-
-@router.get("/scores/{score_id}", response_model=Optional[ScoreOut])
-def get_one_score(
-    score_id: int,
-    response: Response,
-    repo: ScoreRepository = Depends(),
-) -> ScoreOut:
-    score = repo.get_one_score(score_id)
-    if score is None:
-        response.status_code = 404
-    return score
