096200c046ad726b0e15a88c84a684387b5201b3
Jorge <landerosjorge8@outlook.com>
2023-07-26 10:18:12 -0700

    testing deployment pt 9

--- api/routers/scores.py
+++ api/routers/scores.py
@@ -12,22 +12,19 @@ router = APIRouter()
 
 @router.post("/api/scores", response_model=Union[ScoreOut, Error])
 def create_score(
-    score: ScoreIn,
-    response: Response,
-    repo: ScoreRepository = Depends(),
-    account_data: dict = Depends(authenticator.get_current_account_data),
-    ):
+        score: ScoreIn,
+        response: Response,
+        repo: ScoreRepository = Depends(),
+        account_data: dict = Depends(authenticator.get_current_account_data),
+):
     response.status_code = 200
     return repo.create(score)
-
-
 
 @router.get("/api/scores", response_model=Union[List[ScoreOut], Error])
 def get_all_scores(
     repo: ScoreRepository = Depends(),
 ):
     return repo.get_all()
-
 
 @router.delete("/api/scores/{score_id}", response_model=bool)
 def delete_score(
